55e1030554f466081ac28358f616e2b3b514c676
Prabal2207 <prabal760@gmail.com>
2021-07-24 23:43:49 +0530

    [Prabal] Added:-This is main file

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to user registration!");
+
+            string FirstName;
+            Console.Write("Enter First Name: ");
+            FirstName = Console.ReadLine();
+
+            UserRegistration.checkFirstName(FirstName);
+
+        }
+    }
+}
